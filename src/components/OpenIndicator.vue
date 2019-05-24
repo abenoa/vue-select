3d65c4d19de040946a755bc1a201c46fe6774c96
Rodrigo Waltenberg <rwaltenberg@gmail.com>
2019-05-24 11:23:50 -0300

    change arrow svg

--- src/components/OpenIndicator.vue
+++ src/components/OpenIndicator.vue
@@ -1,5 +1,7 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="10">
-    <path d="M9.211364 7.59931l4.48338-4.867229c.407008-.441854.407008-1.158247 0-1.60046l-.73712-.80023c-.407008-.441854-1.066904-.441854-1.474243 0L7 5.198617 2.51662.33139c-.407008-.441853-1.066904-.441853-1.474243 0l-.737121.80023c-.407008.441854-.407008 1.158248 0 1.600461l4.48338 4.867228L7 10l2.211364-2.40069z"/>
+  <svg width="14px" height="8px" viewBox="0 0 14 8" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" stroke="#000000" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <g id="Arrow">
+          <polyline id="Path" points="1 1 7 7 13 1"></polyline>
+      </g>
   </svg>
 </template>
